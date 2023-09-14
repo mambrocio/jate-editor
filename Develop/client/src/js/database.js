e841cf4eb18445485ecdb51e682e74331379da5d
marc ambrocio <mikedbambrocio@gmail.com>
2023-09-14 11:04:09 -0400

    Finished Todos, will cleanup files and add more

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,46 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+
+export const putDb = async (content) => {
+  console.log('DB Item Added');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, text: content });
+
+  const result = await request;
+
+  console.log ('This Item Has Been Saved', result);
+
+};
+
+
+export const getDb = async () => {
+  console.log('Retrieve from database');
+
+
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+
+  console.log("Editor Value", result);
+  return result;
+}
+
+initdb();
+
